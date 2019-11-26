610c64d549ecbaadbcb2956ebd8419311149bd5e
briannemsick <brian.nemsick@gmail.com>
2019-11-26 15:17:48 -0800

    text

--- ml/text.py
+++ ml/text.py
@@ -14,7 +14,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-
 bow = TfidfVectorizer().fit(X_train)
 X_train_bow = bow.transform(X_train)
 X_test_bow = bow.transform(X_test)
