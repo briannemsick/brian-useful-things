8d631809ce01114077eb280d583019da8da5ee5d
briannemsick <brian.nemsick@gmail.com>
2019-11-26 13:59:17 -0800

    precommit

--- ml/mnist.py
+++ ml/mnist.py
@@ -2,7 +2,6 @@ import numpy as np
 import tensorflow as tf
 from sklearn.metrics import accuracy_score
 
-
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
 X_train = X_train[:, ..., np.newaxis] / 255.0
 X_test = X_test[:, ..., np.newaxis] / 255.0
@@ -16,11 +15,15 @@ model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(units=128, activation="relu"))
 model.add(tf.keras.layers.Dense(units=10, activation="softmax"))
 
-model.compile(optimizer="Adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+model.compile(
+    optimizer="Adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+)
 
 callbacks = [tf.keras.callbacks.ModelCheckpoint("model_best.h5")]
 
-model.fit(X_train, y_train, validation_split=0.2, callbacks=callbacks, epochs=1, batch_size=32)
+model.fit(
+    X_train, y_train, validation_split=0.2, callbacks=callbacks, epochs=1, batch_size=32
+)
 model.load_weights("model_best.h5")
 y_test_hat = model.predict(X_test)
 print(accuracy_score(np.argmax(y_test_hat, axis=1), y_test))
